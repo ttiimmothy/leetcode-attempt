2d74a6a464813e1e9feb168aef5331f488d60442
ttiimmothy <dinosauli2006@gmail.com>
2024-03-30 11:51:39 -0400

    feat add rsut code and update rust code syntax

---
+++ algorithm/contexts/rust1.rs
@@ -0,0 +1,9 @@
+fn main() {
+  // rustc icon unicode
+  println!("{:x}", '' as u32);
+  // golang icon unicode
+  println!("{:x}", '' as u32);
+  // google cloud icon unicode
+  println!("{:x}", '' as u32);
+  
+}
