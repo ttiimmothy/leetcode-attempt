6565e215f654d14e2cf4f586f0861479130c197d
ttiimmothy <dinosauli2006@gmail.com>
2024-04-03 18:11:23 -0400

    misc add multiple objective c++ files

---
+++ object.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for i in {1..80}; do
+  printf -v padded "%03d" "$i"  # Format the number with leading zeros
+  # echo "object${padded}.mm"
+  # echo "object${i}.mm"
+  # echo "object\"${padded}\".mm"
+  cp object.mm objectiveC++/object${padded}.mm
+done
