3ae5e89441194cae8bf76d62a876f94ca61f13ca
ttiimmothy <dinosauli2006@gmail.com>
2024-04-02 20:18:28 -0400

    feat update all codes syntax and remove useless codes

--- algorithm/code/main.go
+++ algorithm/code/main.go
@@ -8,10 +8,6 @@ import (
 func main() {
 	fmt.Println("a")
 	array := []int{-4, 0, 7, 4, 9, -5, -1, 0, -7, -3}
-<<<<<<< Updated upstream
 	// src.heapSort(array)
-=======
-	// src.HeapSort(array)
->>>>>>> Stashed changes
 	fmt.Println(array)
 }
